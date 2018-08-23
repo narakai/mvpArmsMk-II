6f4b057f6cd1a6b2636b3fb13de05ae834de957a
laileon <leon@igola.com>
2018-08-23 23:21:52 +0800

    尝试data binding, 取代butter knife

--- app/src/main/java/clem/app/musicplayer/MainActivity.java
+++ app/src/main/java/clem/app/musicplayer/MainActivity.java
@@ -1,13 +1,14 @@
 package clem.app.musicplayer;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import clem.app.musicplayer.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //添加布局，ActivityMainBinding命名方式是根据布局文件的命名方式来的
+        ActivityMainBinding binding = DataBindingUtil.setContentView(MainActivity.this,R.layout.activity_main);
+        setSupportActionBar(binding.toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
