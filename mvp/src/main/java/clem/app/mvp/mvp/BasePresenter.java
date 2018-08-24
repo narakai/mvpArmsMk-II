1e51f71cbfaec45b3d1b6050b427678bfabfe164
laileon <leon@igola.com>
2018-08-24 20:25:00 +0800

    skeleton

---
+++ mvp/src/main/java/clem/app/mvp/mvp/BasePresenter.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2017 JessYan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package clem.app.mvp.mvp;
+
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleOwner;
+
+import com.uber.autodispose.AutoDisposeConverter;
+
+import org.jetbrains.annotations.NotNull;
+
+import clem.app.mvp.utils.Preconditions;
+import clem.app.mvp.utils.RxLifecycleUtils;
+
+/**
+ * ================================================
+ * 基类 Presenter
+ *
+ * @see <a href="https://github.com/JessYanCoding/MVPArms/wiki#2.4.4">Presenter wiki 官方文档</a>
+ * Created by JessYan on 4/28/2016
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public class BasePresenter<M extends IModel, V extends IView> implements IPresenter {
+    protected final String TAG = this.getClass().getSimpleName();
+
+    protected M mModel;
+    protected V mRootView;
+    private LifecycleOwner lifecycleOwner;
+
+    /**
+     * 如果当前页面同时需要 Model 层和 View 层,则使用此构造函数(默认)
+     *
+     * @param model
+     * @param rootView
+     */
+    public BasePresenter(M model, V rootView) {
+        Preconditions.checkNotNull(model, "%s cannot be null", IModel.class.getName());
+        Preconditions.checkNotNull(rootView, "%s cannot be null", IView.class.getName());
+        this.mModel = model;
+        this.mRootView = rootView;
+    }
+
+    /**
+     * 如果当前页面不需要操作数据,只需要 View 层,则使用此构造函数
+     *
+     * @param rootView
+     */
+    public BasePresenter(V rootView) {
+        Preconditions.checkNotNull(rootView, "%s cannot be null", IView.class.getName());
+        this.mRootView = rootView;
+    }
+
+
+    protected <T> AutoDisposeConverter<T> bindLifecycle() {
+        if (null == lifecycleOwner)
+            throw new NullPointerException("lifecycleOwner == null");
+        return RxLifecycleUtils.bindLifecycle(lifecycleOwner);
+    }
+
+
+    @Override
+    public void setLifecycleOwner(LifecycleOwner lifecycleOwner) {
+        this.lifecycleOwner = lifecycleOwner;
+    }
+
+    @Override
+    public void onCreate(@NotNull LifecycleOwner owner) {
+
+    }
+
+    @Override
+    public void onStart(@NotNull LifecycleOwner owner) {
+
+    }
+
+    @Override
+    public void onResume(@NotNull LifecycleOwner owner) {
+
+    }
+
+    @Override
+    public void onPause(@NotNull LifecycleOwner owner) {
+
+    }
+
+    @Override
+    public void onStop(@NotNull LifecycleOwner owner) {
+
+    }
+
+    @Override
+    public void onDestroy(@NotNull LifecycleOwner owner) {
+        if (mModel != null)
+            mModel.onDestroy();
+        this.mModel = null;
+        this.mRootView = null;
+    }
+
+    @Override
+    public void onLifecycleChanged(@NotNull LifecycleOwner owner, Lifecycle.@NotNull Event event) {
+
+    }
+
+
+}
---
+++ mvp/src/main/java/clem/app/mvp/mvp/IPresenter.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2017 JessYan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package clem.app.mvp.mvp;
+
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.OnLifecycleEvent;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * ================================================
+ * 框架要求框架中的每个 Presenter 都需要实现此类,以满足规范
+ *
+ * @see BasePresenter
+ * @see <a href="https://github.com/JessYanCoding/MVPArms/wiki#2.4.4">Presenter wiki 官方文档</a>
+ * Created by JessYan on 4/28/2016
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+public interface IPresenter extends LifecycleObserver {
+
+    void setLifecycleOwner(LifecycleOwner lifecycleOwner);
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    void onCreate(@NotNull LifecycleOwner owner);
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    void onStart(@NotNull LifecycleOwner owner);
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    void onResume(@NotNull LifecycleOwner owner);
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    void onPause(@NotNull LifecycleOwner owner);
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    void onStop(@NotNull LifecycleOwner owner);
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    void onDestroy(@NotNull LifecycleOwner owner);
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+    void onLifecycleChanged(@NotNull LifecycleOwner owner,
+                            @NotNull Lifecycle.Event event);
+
+}
