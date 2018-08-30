ee82d13a9c424f4f6573fbafe9ce93ec13ae96e6
laileon <leon@igola.com>
2018-08-30 22:11:46 +0800

    good so far

---
+++ app/src/main/java/clem/app/musicplayer/mvp/presenter/UserPresenter.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2017 JessYan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package clem.app.musicplayer.mvp.presenter;
+
+/**
+ * ================================================
+ * 展示 Presenter 的用法
+ *
+ * @see <a href="https://github.com/JessYanCoding/MVPArms/wiki#2.4.4">Presenter wiki 官方文档</a>
+ * Created by JessYan on 09/04/2016 10:59
+ * <a href="mailto:jess.yan.effort@gmail.com">Contact me</a>
+ * <a href="https://github.com/JessYanCoding">Follow me</a>
+ * ================================================
+ */
+//@ActivityScope
+//public class UserPresenter extends BasePresenter<UserContract.Model, UserContract.View> {
+//    @Inject
+//    RxErrorHandler mErrorHandler;
+//    @Inject
+//    AppManager mAppManager;
+//    @Inject
+//    ModuleApplication mApplication;
+//    private int lastUserId = 1;
+//    private boolean isFirst = true;
+//    private int preEndIndex;
+//
+//
+//    @Inject
+//    public UserPresenter(UserContract.Model model, UserContract.View rootView) {
+//        super(model, rootView);
+//    }
+//
+//    /**
+//     * 使用 2017 Google IO 发布的 Architecture Components 中的 Lifecycles 的新特性 (此特性已被加入 Support library)
+//     * 使 {@code Presenter} 可以与 {@link SupportActivity} 和 {@link Fragment} 的部分生命周期绑定
+//     */
+//    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+//    void onCreate() {
+//        requestUsers(true);//打开 App 时自动加载列表
+//    }
+//
+//    public void requestUsers(final boolean pullToRefresh) {
+//        //请求外部存储权限用于适配android6.0的权限管理机制
+//        PermissionUtil.externalStorage(new PermissionUtil.RequestPermission() {
+//            @Override
+//            public void onRequestPermissionSuccess() {
+//                //request permission success, do something.
+//                requestFromModel(pullToRefresh);
+//            }
+//
+//            @Override
+//            public void onRequestPermissionFailure(List<String> permissions) {
+//                mRootView.showMessage("Request permissions failure");
+//                mRootView.hideLoading();//隐藏下拉刷新的进度条
+//            }
+//
+//            @Override
+//            public void onRequestPermissionFailureWithAskNeverAgain(List<String> permissions) {
+//                mRootView.showMessage("Need to go to the settings");
+//                mRootView.hideLoading();//隐藏下拉刷新的进度条
+//            }
+//        }, mRootView.getRxPermissions(), mErrorHandler);
+//    }
+//
+//    private void requestFromModel(boolean pullToRefresh) {
+//
+//    }
+//}
